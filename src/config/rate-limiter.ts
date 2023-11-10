9f5836e2179d9653aeaab527f2dd9134786ac716
Kain Nhantumbo <nhantumbok@gmail.com>
2023-11-10 19:40:24 +0200

    feat: added rate limiter config, cors and event logger

---
+++ src/config/rate-limiter.ts
@@ -0,0 +1,18 @@
+import EventLogger from '../lib/event-logger';
+import { rateLimit } from 'express-rate-limit';
+
+export const rateLimiter = rateLimit({
+  windowMs: 60 * 1000,
+  max: 200,
+  message: 'Too many requests from your IP, please try again after 60 seconds.',
+  statusCode: 429,
+  standardHeaders: true,
+  legacyHeaders: false,
+  handler: function (req, res, next, options) {
+    const event = new EventLogger({
+      message: `${options.message}\t${req.method}\t${req.url}\t${req.headers.origin}`,
+      fileName: 'rate-limiter.log'
+    });
+    event.register();
+  }
+});
