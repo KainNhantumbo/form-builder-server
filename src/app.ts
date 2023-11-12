32681c1d247a8701ae0f19238e6a81a9e3d8d7c4
Kain Nhantumbo <nhantumbok@gmail.com>
2023-11-12 17:40:01 +0200

    feat: added route paths

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,12 @@ import cookieParser from 'cookie-parser';
 import { corsOptions } from './config/cors';
 import CreateServer from './modules/server';
 import { rateLimiter } from './config/rate-limiter';
+import { _AuthRoutes } from './routes/auth.routes';
+import { _UserRoutes } from './routes/user.routes';
+import { _FormRoutes } from './routes/form.route';
 import { _HealthRoute } from './routes/healthcheck.routes';
+import { _SubmissionsRoutes } from './routes/submissions.route';
+
 //server configuration
 dotenv.config();
 const app = express();
@@ -20,5 +25,9 @@ app.use(express.json({ limit: '2mb' }));
 app.use(cookieParser());
 
 app.use('/api/v1/healthcheck', _HealthRoute);
+app.use('/api/v1/auth', _AuthRoutes);
+app.use('/api/v1/users', _UserRoutes);
+app.use('/api/v1/forms', _FormRoutes);
+app.use('/api/v1/submissions', _SubmissionsRoutes);
 
 new CreateServer({ app, port });
